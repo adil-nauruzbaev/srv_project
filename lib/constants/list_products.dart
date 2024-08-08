3177a58808387b4516709c087e226d5bba98b703
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-08 17:15:06 +0500

    List of test products (changeable)

---
+++ lib/constants/list_products.dart
@@ -0,0 +1,24 @@
+import 'package:srv_project/models/product.dart';
+
+List<Product> productsToInsert = [
+  Product(
+      name: 'Футболка',
+      description:
+          'Базовый элемент гардероба, имеет короткие или длинные рукава',
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Blue_Tshirt.jpg/250px-Blue_Tshirt.jpg'),
+  Product(
+      name: 'Рубашка',
+      description: 'Состоит из воротника, полочек, спинки, кокетки',
+      imageUrl: 'https://files.gifts.ru/reviewer/tb/58/1836.14_9_500.jpg'),
+  Product(
+      name: 'Шляпа',
+      description: 'Головной убор',
+      imageUrl:
+          'https://static3.depositphotos.com/1001449/153/i/450/depositphotos_1531740-stock-photo-hat.jpg'),
+  Product(
+      name: 'Галстук',
+      description: 'Особым образом сшитая полоса ткани, завязанная вокруг шеи',
+      imageUrl:
+          'https://main-cdn.sbermegamarket.ru/big1/hlr-system/15743211121/600000724036b0.jpeg'),
+];
