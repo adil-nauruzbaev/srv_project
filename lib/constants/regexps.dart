b248a5375df424a53aacc83257c61499bfb958ff
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-09 15:43:07 +0500

    Fixed warnings

--- lib/constants/regexps.dart
+++ lib/constants/regexps.dart
@@ -1,7 +1,9 @@
 // Пример адреса электронной почты: example.email@example-domain.com
+// ignore: non_constant_identifier_names
 final RegExp EMAIL_VALIDATION_REGEX =
     RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
 
 // Пример пароля: Password1
+// ignore: non_constant_identifier_names
 final RegExp PASSWORD_VALIDATION_REGEX =
     RegExp(r"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
