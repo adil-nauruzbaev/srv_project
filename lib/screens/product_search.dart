b291beef318ca9d3a22c2b45c5d4842d3bcb0834
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-09 15:44:40 +0500

    Search for products

---
+++ lib/screens/product_search.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/product.dart';
+import '../providers/product_provider.dart';
+
+class ProductSearchDelegate extends SearchDelegate<Product?> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Consumer(builder: (context, ref, _) {
+      final productList = ref.watch(productProvider);
+      final results = productList.when(
+        data: (products) => products
+            .where((product) =>
+                product.name.toLowerCase().contains(query.toLowerCase()))
+            .toList(),
+        loading: () => [],
+        error: (err, stack) => [],
+      );
+
+      return ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (context, index) {
+          final product = results[index];
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text(product.description),
+            leading: SizedBox(
+              width: 60,
+              height: 60,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+            onTap: () {
+              close(context, product);
+            },
+          );
+        },
+      );
+    });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return buildResults(context);
+  }
+}
